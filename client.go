08a41245b2ff9cb7fd2ab0675698c80e29e4eacb
Santhosh Puvaneswaran <santhosh.puvaneswaran@gmail.com>
2021-04-09 09:07:37 +0530

    changed the port from 9000 to 9001

--- client.go
+++ client.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	conn, err := grpc.Dial(":9001", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("could not connect: %s", err)
 	}
--- server.go
+++ server.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":9000")
+	lis, err := net.Listen("tcp", ":9001")
 	if err != nil {
-		log.Fatalf("Failed to listen on port 9000: %v", err)
+		log.Fatalf("Failed to listen on port 9001: %v", err)
 	}
 
 	s := chat.Server{}
@@ -21,7 +21,7 @@ func main() {
 	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+		log.Fatalf("Failed to serve gRPC server over port 9001: %v", err)
 	}
 
 }
